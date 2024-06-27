aa4634a719f0b00c5f353613b6de8a72f4804218
William He <williamhedestiny@gmail.com>
2024-06-27 12:05:09 -0700

    starting tic tac toe

---
+++ experimenting/factory.js
@@ -0,0 +1,16 @@
+const User = function (name) {
+    this.name = name;
+    this.discordName = "@" + name;
+  }
+  // hey, this is a constructor - 
+  // then this can be refactored into a factory!
+  
+  function createUser (name) {
+    const discordName = "@" + name;
+    return { name, discordName };
+  }
+  // and that's very similar, except since it's just a function,
+  // we don't need a new keyword
+
+  const bruh = new User('bruh');
+  console.log(bruh.discordName);
